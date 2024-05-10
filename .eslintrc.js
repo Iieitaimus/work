256ec9e2aa214504954a2372a5ef3f7fad208d3c
iieitaimus <dawid092004@onet.pl>
2024-05-10 22:31:52 +0200

    'aa'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,21 @@
 module.exports = {
-	globals: {
-		__webpack_nonce__: true,
-		_: true,
-		$: true,
-		dayNames: true,
-		escapeHTML: true,
-		firstDay: true,
-		moment: true,
-		oc_userconfig: true,
-		sinon: true,
-	},
-	plugins: [
-		'cypress',
-	],
-	extends: [
-		'@nextcloud/eslint-config/typescript',
-		'plugin:cypress/recommended',
-	],
-	rules: {
-		'no-tabs': 'warn',
-		// TODO: make sure we fix this as this is bad vue coding style.
-		// Use proper sync modifier
-		'vue/no-mutating-props': 'warn',
-		'vue/custom-event-name-casing': ['error', 'kebab-case', {
-			// allows custom xxxx:xxx events formats
-			ignores: ['/^[a-z]+(?:-[a-z]+)*:[a-z]+(?:-[a-z]+)*$/u'],
-		}],
-	},
-	settings: {
-		jsdoc: {
-			mode: 'typescript',
-		},
-	},
-}
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': [2, { args: 'all', argsIgnorePattern: '^_' }],
+  },
+};
